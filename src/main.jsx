b81abc456728b6326b7f456353ff44d4b1ef1418
SudhakaranV17 <44158979+SudhakaranV17@users.noreply.github.com>
2024-09-03 20:53:24 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import React from "react";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
